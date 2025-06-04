185aeca606de9a6a343c132731f6391f2ddb95bc
Ayeman <afouad@dons.usfca.edu>
2025-06-04 12:08:56 -0700

    update

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,10 +1,11 @@
 import java.lang.util*
 
-class Main{
+class Main
+  {
 
 public static void main(String[] args){
 
-System.out.println("Hello World");
+   System.out.println("Hello World");
 
-}
+  }
 }
